e77f27853e6b517c3428e86d3e4942f77d48fbec
Billngoga <ngogaabill@example.com>
2025-11-13 18:18:51 -0600

    Wrote StreetTaco Class

--- src/main/java/com/pluralsight/models/StreetTaco.java
+++ src/main/java/com/pluralsight/models/StreetTaco.java
@@ -1,7 +1,29 @@
 package com.pluralsight.models;
 
-public class StreetTaco extends Taco{
-    public StreetTaco(String tortillaType, String tacoSize, boolean deepFried) {
-        super(tortillaType, tacoSize, deepFried);
+public class StreetTaco extends Taco {
+    public StreetTaco() {
+        super();
+        setTacoSize("3-Taco");
+        setTortillaType("Corn");
+        getTacoSizePrice(9.00);
+
+        loadStreetTacoToppings();
+    }
+
+    /**
+     * Load the Topping that come with street taco
+     */
+    private void loadStreetTacoToppings() {
+        PremiumToppings carneAsada = new PremiumToppings("carne Asada", Toppings.MEAT, false, 2.0, 1.0);
+        addTopping(carneAsada);
+        addTopping(new Toppings("Onions", Toppings.REGULAR, false));
+        addTopping(new Toppings("cilantro", Toppings.REGULAR, false));
+        addTopping(new Toppings("salsa verde", Toppings.SAUCE, false));
+        addTopping(new Toppings("Lime wedges", Toppings.SIDE, false));
+    }
+
+    @Override
+    public String toString() {
+        return "Street Taco " + super.toString();
     }
 }
